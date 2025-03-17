8f021d5cc2e764fae334acf74d0e0fd6d07f16a5
Alexeev <sergey50878@mail.ru>
2025-03-17 21:06:49 +0200

    add match dao and team dao

--- src/main/java/org/moodle/springlaboratorywork/repository/hibernateRepository/MatchDao.java
+++ src/main/java/org/moodle/springlaboratorywork/repository/hibernateRepository/MatchDao.java
@@ -1,4 +1,116 @@
 package org.moodle.springlaboratorywork.repository.hibernateRepository;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.moodle.springlaboratorywork.entity.Match;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MatchDao {
+
+    public List<Match> findAll() {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            //todo write hoin for getting home and away teams
+            //String hql = "select m from Match m join fetch l.teams";
+            Query query = session.createQuery(hql, Match.class);
+
+            List<Match> matches = query.getResultList();
+
+            transaction.commit();
+            return matches;
+        }catch (Exception e){
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public Optional<Match> findById(Long id) {
+        Match match = null;
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            match = session.get(Match.class, id);
+
+            transaction.commit();
+            return Optional.ofNullable(match);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+
+        }
+    }
+
+    public Match save(Match match) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.persist(match);
+
+            transaction.commit();
+            return match;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public Match update(Match match) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.merge(match);
+
+            transaction.commit();
+            return match;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public void delete(Match match){
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.remove(match);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public Boolean existsByName(String name) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String hql = "SELECT CASE WHEN COUNT(l) > 0 THEN true ELSE false END FROM Match l WHERE l.name = :name";
+            Query query = session.createQuery(hql).setParameter("name", name);
+            transaction.commit();
+            return (Boolean) query.getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/main/java/org/moodle/springlaboratorywork/repository/hibernateRepository/TeamDao.java
+++ src/main/java/org/moodle/springlaboratorywork/repository/hibernateRepository/TeamDao.java
@@ -1,4 +1,117 @@
 package org.moodle.springlaboratorywork.repository.hibernateRepository;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.moodle.springlaboratorywork.entity.Team;
+import org.moodle.springlaboratorywork.entity.Team;
+
+import java.util.List;
+import java.util.Optional;
+
 public class TeamDao {
+
+
+    public Optional<Team> findById(Long id) {
+        Team team = null;
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            team = session.get(Team.class, id);
+
+            transaction.commit();
+            return Optional.ofNullable(team);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+
+        }
+    }
+    //todo realazie fetching
+    public List<Team> findAll() {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            //String hql = "select t from Team t join fetch l.teams";
+            Query query = session.createQuery(hql, Team.class);
+
+            List<Team> teams = query.getResultList();
+
+            transaction.commit();
+            return teams;
+        }catch (Exception e){
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public Team save(Team team) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.persist(team);
+
+            transaction.commit();
+            return team;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public Team update(Team team) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.merge(team);
+
+            transaction.commit();
+            return team;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public void delete(Team team){
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            session.remove(team);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public Boolean existsByName(String name) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String hql = "SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END FROM Team t WHERE t.name = :name";
+            Query query = session.createQuery(hql).setParameter("name", name);
+            transaction.commit();
+            return (Boolean) query.getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
